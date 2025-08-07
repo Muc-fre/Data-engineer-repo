d66390557429a9505265f0b60d28cac3a603ed46
muc-fre <mucyofred6@gmail.com>
2025-08-07 03:14:20 -0400

    another table

--- instructor-lab/db_code.py
+++ instructor-lab/db_code.py
@@ -6,8 +6,10 @@ conn=sqlite3.connect('STAFF.db')
 table_name = 'INSTRUCTOR'
 attribute_list=['ID','FNAME','LNAME','CITY','CCODE']
 
-file_path='/home/project/INSTRUCTOR.csv'
+
+file_path='/home/project/instructor-lab/INSTRUCTOR.csv'
 df=pd.read_csv(file_path,names=attribute_list)
+
 
 df.to_sql(table_name, conn, if_exists = 'replace', index =False)
 print('Table is ready')
@@ -44,5 +46,30 @@ data_append = pd.DataFrame(data_dict)
 data_append.to_sql(table_name, conn, if_exists = 'append', index =False)
 print('Data appended successfully')
 
+# assignment
+
+table_name2='Departments'
+attribute_list2=['DEPT_ID','DEP_NAME','MANAGER_ID','LOC_ID']
+
+csv_path = "/home/project/instructor-lab/Departments.csv"
+df1=pd.read_csv(csv_path,names=attribute_list2)
+
+df1.to_sql(table_name2, conn, if_exists = 'replace', index =False)
+print('Table is ready')
+
+new_department = {
+    'DEPT_ID': [9],
+    'DEP_NAME': ['Quality Assurance'],
+    'MANAGER_ID': [30010],
+    'LOC_ID': ['L0010']
+}
+data_appended = pd.DataFrame(new_department)
+data_appended.to_sql(table_name2, conn, if_exists = 'append', index =False)
+print('Data appended successfully')
+
+query2 = f"SELECT * FROM {table_name2}"
+query2_output = pd.read_sql(query2, conn)
+print(query2)
+print(query2_output)
 
 conn.close()
